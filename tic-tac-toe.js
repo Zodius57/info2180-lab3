f37138844a21ed7696ec4e6dafdab07186fc5806
Kale Williams <klaeboy.williams@gmail.com>
2024-10-27 20:44:55 -0500

    Added X and O that cant be cheated

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let turn = 1;
+    let boMem = Array(9).fill(null);
+    let setup = 0;
+
+    const setX = "X";
+    const setO = "O";
+    let currentShape = setX;
+
     let board = document.getElementById("board");
     for (const child of board.children) {
         child.setAttribute("class", "square");
+        child.setAttribute("id", setup);
+        setup+=1;
     }
-});
 
+    let boxes = Array.from(document.getElementsByClassName("square"));
+
+    boxes.forEach(box => box.addEventListener("click", function(){
+        let position = box.id;
+        if(!boMem[position]){
+            boMem[position] = currentShape;
+            box.innerText = currentShape;
+
+            if(currentShape == setX){
+                box.setAttribute("class", "square X");
+                currentShape = setO;
+                turn+= 1;
+            }else{
+                box.setAttribute("class", "square O");
+                currentShape = setX;
+                turn+= 1;
+            }
+        }
+    }))
+    
+});
 
 
